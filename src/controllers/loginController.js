6a30f6c4802791099d694a509b24283a6f99eb13
Wesley Ribeiro <wesleyaraujo@cc.ci.ufpb.br>
2021-08-21 14:26:44 -0300

    criando usuários

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,3 +1,27 @@
+const Login = require('../models/LoginModel');
+
 exports.index = (req, res) => {
   res.render('login')
+}
+
+exports.register = async function (req, res) {
+  try {
+    const login = new Login(req.body)
+    await login.register()
+    console.log(login)
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors)
+      req.session.save(function () {
+        return res.redirect('/login');
+      })
+      return
+    }
+    req.flash('success', 'Usuário criado com sucesso')
+    req.session.save(function () {
+      return res.redirect('/login');
+    })
+  } catch (e) {
+    console.log(e)
+    return res.render('404')
+  }
 }
---
+++ src/models/LoginModel.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcryptjs = require('bcryptjs')
+
+const LoginSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const LoginModel = mongoose.model('Login', LoginSchema);
+
+class Login {
+  constructor(body) {
+    this.body = body;
+    this.errors = [];
+    this.user = null;
+  }
+
+  async register() {
+    this.valida()
+    if (this.errors.length > 0) return
+
+    await this.userExists()
+    if (this.errors.length > 0) return
+
+    const salt = bcryptjs.genSaltSync()
+    this.body.password = bcryptjs.hashSync(this.body.password, salt)
+
+    try {
+      this.user = await LoginModel.create(this.body)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  valida() {
+    this.cleanUp()
+    //validação e-mail
+    if (!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido')
+    //validação senha
+    if (this.body.password.length < 3 || this.body.password.length >= 50) this.errors.push('A senha precisa ter entre 3 e 50 caracteres')
+  }
+
+  async userExists() {
+    const user = await LoginModel.findOne({ email: this.body.email })
+    if (user) this.errors.push('E-mail já registrado')
+  }
+
+  cleanUp() {
+    for (const key in this.body) {
+      if (typeof this.body[key] !== "string") this.body[key] = ''
+    }
+    this.body = {
+      email: this.body.email,
+      password: this.body.password
+    }
+  }
+}
+
+module.exports = Login;
